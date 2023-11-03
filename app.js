ef5628c0844c500b7e6f53f377da2de112db4afa
Yusmin97 <tmdamsi97@gmail.com>
2023-11-03 11:50:07 +0900

    라우터 따로 관리 라우터 파일 연결 완료

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 const path = require('path');
 const app = express();
 const port = 5050;
@@ -8,10 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중 입니다.`)
