057fb2162b9a8058d09f7e72de2f3b67b7a8c5ed
Yusmin97 <tmdamsi97@gmail.com>
2023-11-03 12:47:30 +0900

    변수 라우터 이름 변경

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const routesC = require('./routes');
 const path = require('path');
 const app = express();
 const port = 5050;
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
+app.use('/', routesC);
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중 입니다.`)
