809e3432895752aee127df76c85826491723aa8c
Yusmin97 <tmdamsi97@gmail.com>
2023-11-12 16:04:00 +0900

    id값 기능 태그 변수 이름 지정

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,8 @@
-const rightLog = document.getElementById('rightLog');
-const messege = document.getElementById('messege');
+document.addEventListener('DOMContentLoaded', () => {
+  let messageForm = document.getElementById('messageForm');
+  let message = document.getElementById('message');
+  let mainDiv = document.getElementById('main');
+  let hamburgerButton = document.querySelector('.hambugerBtn');
+  let sidebar = document.querySelector('.side');
+
+})
