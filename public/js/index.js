f7d9ded6ae61be8b79ace9e07d94e8c135188ecd
Yusmin97 <tmdamsi97@gmail.com>
2023-11-13 18:05:35 +0900

    messageForm 이벤트 지정과 햄버거 버튼 메뉴 이벤트 지정

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let messageForm = document.getElementById('messageForm');
-  let message = document.getElementById('message');
-  let mainDiv = document.getElementById('main');
-  let hamburgerButton = document.querySelector('.hambugerBtn');
+  let messageForm = document.getElementById('messegeForm');
+  let message = document.getElementById('messege');
+  let rightDiv = document.getElementById('rightLog');
+  let hamburgerBtn = document.querySelector('.hambugerBtn');
   let sidebar = document.querySelector('.side');
 
+  messageForm.addEventListener('submit', () => {
+
+    let messageText = message.ariaValueMax;
+    if (messageText) {
+      let messageLi = document.createElement('li');
+
+      messageLi.textContent = messageText;
+
+      rightDiv.appendChild(messageLi)
+    }
+  })
+  hamburgerBtn.addEventListener('click', () => {
+    if (sidebar.style.width === '100px') {
+      sidebar.style.width = '0';
+    } else { 
+      sidebar.style.width = '100px';
+    }
+  })
 })
