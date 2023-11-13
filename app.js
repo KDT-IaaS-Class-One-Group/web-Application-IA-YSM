b520bd4625a45c7d8319ab8cf5b145f1a85ce6d0
Yusmin97 <tmdamsi97@gmail.com>
2023-11-13 18:01:48 +0900

    라우트 json 파일 모듈화 불러오기

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const path = require('path');
 const app = express();
 const port = 5050;
 
+const { dataJsonS } = require('./routes')
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/', routesC);
+
+app.post('/submit', dataJsonS);
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중 입니다.`)
