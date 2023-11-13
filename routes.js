e6af73cda93150790c8b3bdb4f07c3bd0f09f736
Yusmin97 <tmdamsi97@gmail.com>
2023-11-13 18:01:17 +0900

    json 파일 파싱  후 readfile과 write파일 지정

--- routes.js
+++ routes.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const path = require('path');
+const fs = require('fs');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+router.post('/submit', (req, res) => {
+  let logMessage = req.body.message;
+  console.log(logMessage)
+
+  fs.readFile('./data/data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('An error has occured while loading files')
+      return;
+    }
+
+    let dataJson = JSON.parse(data);
+    let timestamp = new Date().toLocaleTimeString();
+
+    dataJson.mainContent.inputRecords.push({
+      type: 'user',
+      message: logMessage,
+      timestamp: timestamp
+    });
+
+    fs.writeFile('./data/data.json', JSON.stringify(dataJson, null, 2), (err) => {
+      if (err) {
+        console.error('An error has occured while saving files.')
+        return res.status(500).send('Server error')
+      } else {
+        console.log('data has successfully saved.')
+      }
+    })
+  })
 })
 
-module.exports = router;
+module.exports = {
+  dataJsonS : router.post('/submit')
+};
