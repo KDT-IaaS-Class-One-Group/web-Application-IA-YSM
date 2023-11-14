24896115168f02548d5c86648cfb6a20bccc7cc2
Yusmin97 <tmdamsi97@gmail.com>
2023-11-14 10:47:31 +0900

    JSON 메세지 문제 해결(input에 name이 문제였다)

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let messageForm = document.getElementById('messegeForm');
-  let message = document.getElementById('messege');
+  let messageForm = document.getElementById('messageForm');
+  let message = document.getElementById('message');
   let rightDiv = document.getElementById('rightLog');
   let hamburgerBtn = document.querySelector('.hambugerBtn');
   let sidebar = document.querySelector('.side');
 
   messageForm.addEventListener('submit', () => {
 
-    let messageText = message.ariaValueMax;
+    let messageText = message.value;
     if (messageText) {
       let messageLi = document.createElement('li');
-
+      messageLi.style.listStyleType = 'none'
       messageLi.textContent = messageText;
 
       rightDiv.appendChild(messageLi)
